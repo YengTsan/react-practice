afcadaea7961d45d76bfa5f82cd7b508c1948f3f
Yeng Tsan <yengttt@gmail.com>
2018-08-08 16:24:01 +0800

    The component evolve from function component to class based component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // let me access the React
-import React from 'react';
+import React, { Component } from 'react';
 // React manage the data, react DOM is in charge of the HTML rendering
 import ReactDOM from 'react-dom';
 
@@ -9,18 +9,26 @@ import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search'
 const API_KEY = 'AIzaSyBJODiOD_9Jab6-mezCwKdNAs6yeterX8I'
 
-YTSearch({key: API_KEY, term: 'surboards'}, (data) => {
-  console.log(data)
-})
-
 // Create a new component. This component should produce some HTML
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surboards'}, (videos) => {
+      this.setState({ videos })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
