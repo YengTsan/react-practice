66c69c06851768dead90b460e6805e676e4d76e6
Yeng Tsan <yengttt@gmail.com>
2018-08-08 10:34:28 +0800

    Install Youtube API

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 import React from 'react';
 // React manage the data, react DOM is in charge of the HTML rendering
 import ReactDOM from 'react-dom';
+// set the unchange API key
+const API_KEY = 'AIzaSyBWXIx3IbZRMPqB1fVPtv7MCYLuW-DP8R4'
 
 // Create a new component. This component should produce some HTML
 
