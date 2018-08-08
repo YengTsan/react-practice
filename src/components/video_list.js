726c1ea941b0ff45f398409fd60d58fbe8ba25a7
Yeng Tsan <yengttt@gmail.com>
2018-08-08 19:16:13 +0800

    Add item key

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import VideoItem from './video_item';
 
 const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return <VideoItem key={video.etag} video={video} />
+  })
+
   return (
     <ul className="col-md-4 list-group">
-      {props.videos.length}
+      { videoItems }
     </ul>
   )
 }
