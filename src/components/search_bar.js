475b720fde63214ccb027a47e082249018e0219f
Yeng Tsan <yengttt@gmail.com>
2018-08-08 11:14:38 +0800

    Create a Class component

---
+++ src/components/search_bar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+// Component inside the import funciton means pull out the `Component` in the React property
+// and name it to the varaible `Component`
+// const Component = React.Component;
+
+class SearchBar extends Component  {
+  render() {
+    return <input />;
+  }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@
 import React from 'react';
 // React manage the data, react DOM is in charge of the HTML rendering
 import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
 // set the unchange API key
-const API_KEY = 'AIzaSyBWXIx3IbZRMPqB1fVPtv7MCYLuW-DP8R4'
+const API_KEY = 'AIzaSyBJODiOD_9Jab6-mezCwKdNAs6yeterX8I'
 
 // Create a new component. This component should produce some HTML
 
 const App = () => {
-  return <div>Hi!</div>
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
